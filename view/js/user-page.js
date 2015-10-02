09cde7a2712d597893ad1a2136d00db283dbe4c5
jmb <juanmbellini@hotmail.com>
2015-10-02 18:29:52 -0300

    userPage

--- view/js/user-page.js
+++ view/js/user-page.js
@@ -4,3 +4,21 @@
 $(function() {
 	$('.nav-tabs a:first').tab('show');
 });
+
+$(function() {
+	$('#Cart').tab('show');
+});
+
+$(function() {
+	$('#WishList').tab('show');
+});
+
+$(function() {
+	$('#Account').tab('show');
+});
+
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  e.target // newly activated tab
+  e.relatedTarget // previous active tab
+})
