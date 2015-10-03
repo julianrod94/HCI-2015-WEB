aaf47732225d84dbf421f276ecfc3eb9aab3a974
jmb <juanmbellini@hotmail.com>
2015-10-03 02:51:35 -0300

    HeaderSeparado

---
+++ controller/cartCtrl.js
@@ -0,0 +1,24 @@
+angular.module('hciApp', []).controller('cartCtrl', function($scope) {
+    $scope.firstName = "John",
+    $scope.lastName = "Doe",
+   
+
+    $scope.cartLine = {
+    	item : {
+    		name: "Nombre del producto", 
+    		brand:"Marca del producto",
+    		colour:"Color del producto",
+    		size:"Talle del producto",
+    		price:"Precio del producto",
+    		image:"Imagen del producto" 
+    	},
+    	quantity: "Cantidad",
+    	lineTotal: "Total por linea",
+    },
+    $scope.total = "Some total",
+
+     $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+
+});
