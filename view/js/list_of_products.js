0efd811512bd39c67278f087a3263dfdb9ab6442
jmb <juanmbellini@hotmail.com>
2015-10-05 03:29:46 -0300

    catalogoEmpezado

--- view/js/list_of_products.js
+++ view/js/list_of_products.js
@@ -2,16 +2,17 @@
 var products = [
 					{
 						id: 1,
-						prod_id: 1;
+						prod_id: 1,
 						name: 'Brickline',
 						brand: 'DC - Shoes',
 						descriptions: ['Remera básica', '100% algodón'],
 						color: 'Negro',
 						sizes: ['XS', 'S', 'M', 'L'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 99.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 99.99,
 						images: [	
 									'dc_brikline_black_1.jpg',
 									'dc_brikline_black_2.jpg'
@@ -19,16 +20,17 @@ var products = [
 					},
 					{
 						id: 2,
-						prod_id: 1;
+						prod_id: 1,
 						name: 'Brickline',
 						brand: 'DC - Shoes',
 						descriptions: ['Remera básica', '100% algodón'],
 						colors: 'Blanco',
 						sizes: ['XS','S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 99.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 99.99,
 						images: [	
 									'dc_brikline_white1.jpg',
 									'dc_brikline_white2.jpg'
@@ -36,16 +38,17 @@ var products = [
 					},
 					{
 						id: 3,
-						prod_id: 2;
+						prod_id: 2,
 						name: 'DPM DC',
 						brand: 'DC - Shoes',
 						descriptions: ['Remera estampada', '100% algodón'],
 						color: 'Negro',
 						sizes: ['XS','S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 149.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 149.99,
 						images: [	
 									'dc_dpm_black_1.jpg',
 									'dc_dpm_black_2.jpg'
@@ -53,16 +56,17 @@ var products = [
 					},
 					{
 						id: 4,
-						prod_id: 2;
+						prod_id: 2,
 						name: 'DPM DC',
 						brand: 'DC - Shoes',
 						descriptions: ['Remera estampada', '100% algodón'],
 						color: 'Blanco',
 						sizes: ['XS','S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 149.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 149.99,
 						images: [	
 									'dc_dpm_white_1.jpg',
 									'dc_dpm_white_2.jpg'
@@ -70,16 +74,17 @@ var products = [
 					},
 					{
 						id: 5,
-						prod_id: 3;
+						prod_id: 3,
 						name: 'DPM DC',
 						brand: 'DC - Shoes',
 						descriptions: ['100% algodón'],
+						ocassion: 'Casual',
 						color: 'Negro',
 						sizes: ['XS','S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 149.99
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 149.99,
 						images: [	
 									'dc_rebuilt_black_1.jpg',
 									'dc_rebuilt_black_2.jpg'
@@ -87,16 +92,17 @@ var products = [
 					},
 					{
 						id: 6,
-						prod_id: 3;
+						prod_id: 3,
 						name: 'DPM DC',
 						brand: 'DC - Shoes',
 						descriptions: ['100% algodón'],
 						color: 'Blanco',
 						sizes: ['XS','S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 149.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 149.99,
 						images: [	
 									'dc_rebuilt_white_1.jpg',
 									'dc_rebuilt_white_2.jpg'
@@ -104,16 +110,17 @@ var products = [
 					},
 					{
 						id: 7,
-						prod_id: 3;
+						prod_id: 3,
 						name: 'DPM DC',
 						brand: 'DC - Shoes',
 						descriptions: ['100% algodón'],
 						color: 'Verde',
 						sizes: ['S', 'M', 'L'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 149.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 149.99,
 						images: [	
 									'dc_rebuilt_green_1.jpg',
 									'dc_rebuilt_green_2.jpg'
@@ -121,16 +128,17 @@ var products = [
 					},
 					{
 						id: 8,
-						prod_id: 3;
+						prod_id: 3,
 						name: 'DPM DC',
 						brand: 'DC - Shoes',
 						descriptions: ['100% algodón'],
 						color: 'Rojo',
 						sizes: ['S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 149.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 149.99,
 						images: [	
 									'dc_rebuilt_red.jpg',
 									'dc_rebuilt_red.jpg'
@@ -138,16 +146,17 @@ var products = [
 					},
 					{
 						id: 9,
-						prod_id: 4;
+						prod_id: 4,
 						name: 'Regal Artois',
 						brand: 'Vans',
 						descriptions: ['Estampado', '100% algodón'],
 						color: 'Negro',
 						sizes: ['S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 149.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 149.99,
 						images: [	
 									'vans_regal_black_1.jpg',
 									'vans_regal_black_2.jpg',
@@ -156,16 +165,17 @@ var products = [
 					},
 					{
 						id: 10,
-						prod_id: 5;
+						prod_id: 5,
 						name: 'Hyperdry',
 						brand: 'O\'Neill',
 						descriptions: ['Básica', '100% algodón'],
 						color: 'Gris',
 						sizes: ['XS', 'S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 49.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 49.99,
 						images: [	
 									'oneill_hyperdry_grey_1.jpg',
 									'oneill_hyperdry_grey_2.jpg',
@@ -175,16 +185,17 @@ var products = [
 					},
 					{
 						id: 11,
-						prod_id: 5;
+						prod_id: 5,
 						name: 'Hyperdry',
 						brand: 'O\'Neill',
 						descriptions: ['Básica', '100% algodón'],
 						color: 'Negro',
 						sizes: ['XS', 'S', 'M', 'L', 'XL','XXL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Remeras'
-						price: 49.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Remeras',
+						price: 49.99,
 						images: [	
 									'oneill_hyperdry_black_1.jpg',
 									'oneill_hyperdry_black_2.jpg',
@@ -194,16 +205,17 @@ var products = [
 					},
 					{
 						id: 12,
-						prod_id: 6;
+						prod_id: 6,
 						name: 'Blueberry',
 						brand: 'Dinamarca',
 						descriptions: ['Relajado', 'Botones'],
 						color: 'Rojo',
 						sizes: ['S', 'M', 'L', 'XL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Camisas'
-						price: 89.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Camisas',
+						price: 89.99,
 						images: [	
 									'dinamarca_blueberry_red_1.jpg',
 									'dinamarca_blueberry_red_2.jpg'
@@ -211,16 +223,17 @@ var products = [
 					},
 					{
 						id: 13,
-						prod_id: 6;
+						prod_id: 6,
 						name: 'Blueberry',
 						brand: 'Dinamarca',
 						descriptions: ['Relajado', 'Botones'],
 						color: 'Verde',
 						sizes: ['S', 'M', 'L', 'XL'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Camisas'
-						price: 89.99
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Camisas',
+						price: 89.99,
 						images: [	
 									'dinamarca_blueberry_green_1.jpg',
 									'dinamarca_blueberry_green_2.jpg'
@@ -228,16 +241,17 @@ var products = [
 					},
 					{
 						id: 14,
-						prod_id: 7;
+						prod_id: 7,
 						name: 'Regular',
 						brand: 'Lacoste',
 						descriptions: ['Relajado', 'Botones'],
 						color: 'Celeste',
 						sizes: ['38', '40', '42', '44', '46'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Camisas'
-						price: 129.99
+						ocassion: 'Formal',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Camisas',
+						price: 129.99,
 						images: [	
 									'lacoste_regular_lightblue_1.jpg',
 									'lacoste_regular_lightblue_2.jpg'
@@ -245,16 +259,17 @@ var products = [
 					},
 					{
 						id: 15,
-						prod_id: 8;
+						prod_id: 8,
 						name: 'Polo Club',
 						brand: 'Wellington',
 						descriptions: ['Relajado', 'Boton y cierre', '4 Bolsillos'],
 						color: 'Marron',
 						sizes: ['40', '42', '44', '46', '48', '50', '52', '54'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Pantalones'
-						price: 129.99
+						ocassion: 'Sport',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Pantalones',
+						price: 229.99,
 						images: [	
 									'wellington_polo_club_brown_1.jpg',
 									'wellington_polo_club_brown_2.jpg',
@@ -264,16 +279,17 @@ var products = [
 					},
 					{
 						id: 16,
-						prod_id: 8;
+						prod_id: 8,
 						name: 'Polo Club',
 						brand: 'Wellington',
 						descriptions: ['Relajado', 'Boton y cierre', '4 Bolsillos'],
 						color: 'Negro',
 						sizes: ['40', '42', '44', '46', '48', '50', '52', '54'],
-						gender: 'M',
-						category: 'Ind',
-						sub_category: 'Pantalones'
-						price: 129.99
+						ocassion: 'Sport',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Pantalones',
+						price: 229.99,
 						images: [	
 									'wellington_polo_club_black_1.jpg',
 									'wellington_polo_club_black_2.jpg',
@@ -281,7 +297,250 @@ var products = [
 									'wellington_polo_club_black_4.jpg',
 						]
 					},
+					{
+						id: 17,
+						prod_id: 9,
+						name: 'Taxco',
+						brand: 'Keveingstone',
+						descriptions: ['Boton y cierre', '4 Bolsillos'],
+						color: "",
+						sizes: ['38','40', '42', '44', '46', '48'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Bermudas y shorts',
+						price: 99.99,
+						images: [	
+									'kevingstone_taxco_1.jpg',
+									'kevingstone_taxco_2.jpg'
+						]
+					},
+					{
+						id: 18,
+						prod_id: 10,
+						name: 'Bermuda',
+						brand: 'Lacoste',
+						descriptions: ['Relajado', 'Boton y cierre'],
+						color: 'Azul',
+						sizes: ['38','40', '42', '44', '46', '48'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Bermudas y shorts',
+						price: 99.99,
+						images: [	
+									'lacoste_bermuda_blue_1.jpg',
+									'lacoste_bermuda_blue_2.jpg',
+									'lacoste_bermuda_blue_3.jpg',
+						]
+					},
+					{
+						id: 19,
+						prod_id: 10,
+						name: 'Bermuda',
+						brand: 'Lacoste',
+						descriptions: ['Relajado', 'Boton y cierre'],
+						color: 'Blanco',
+						sizes: ['38','40', '42', '44', '46', '48'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Bermudas y shorts',
+						price: 89.99,
+						images: [	
+									'lacoste_bermuda_white_1.jpg',
+									'lacoste_bermuda_white_2.jpg'
+						]
+					},
+					{
+						id: 20,
+						prod_id: 11,
+						name: 'Skinny fit',
+						brand: 'Lacoste',
+						descriptions: ['Relajado', 'Boton y cierre', 'Semi recto'],
+						color: '',
+						sizes: ['30', '32', '34', '36', '38'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Jeans',
+						price: 299.99,
+						images: [	
+									'lacoste_skinny_1.jpg',
+									'lacoste_skinny_2.jpg',
+									'lacoste_skinny_3.jpg',
+									'lacoste_skinny_4.jpg',
+						]
+					},
+					{
+						id: 21,
+						prod_id: 12,
+						name: 'Photog',
+						brand: 'Quicksilver',
+						descriptions: ['Relajado', 'Cordón y belcro'],
+						color: '',
+						sizes: ['30', '32', '34', '36', '38'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Trajes de Baño',
+						price: 79.99,
+						images: [	
+									'quicksilver_photog_1.jpg',
+									'quicksilver_photog_2.jpg'
+						]
+					},
+					{
+						id: 22,
+						prod_id: 13,
+						name: 'Sfida',
+						brand: 'La toscana',
+						descriptions: ['Relajado', 'Botón, cordón y belcro', 'Bolsillo'],
+						color: 'Rosa y negro',
+						sizes: ['30', '32', '34', '36', '38'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Trajes de Baño',
+						price: 59.99,
+						images: [	
+									'tocasna_sfida_pink_1.jpg',
+									'tocasna_sfida_pink_2.jpg',
+									'tocasna_sfida_pink_3.jpg',
+									'tocasna_sfida_pink_4.jpg',
+						]
+					},
+					{
+						id: 23,
+						prod_id: 13,
+						name: 'Sfida',
+						brand: 'La toscana',
+						descriptions: ['Relajado', 'Botón, cordón y belcro', 'Bolsillo'],
+						color: 'Celeste y blanco',
+						sizes: ['30', '32', '34', '36', '38'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Trajes de Baño',
+						price: 59.99,
+						images: [	
+									'tocasna_sfida_lightblue_1.jpg',
+									'tocasna_sfida_lightblue_2.jpg',
+									'tocasna_sfida_lightblue_3.jpg',
+									'tocasna_sfida_lightblue_4.jpg',
+						]
+					},
+					{
+						id: 24,
+						prod_id: 14,
+						name: 'Park Stadium',
+						brand: 'C1RCA',
+						descriptions: ['Botones', 'Apliques bordados'],
+						color: 'Negro y gris',
+						sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Abrigos',
+						price: 159.99,
+						images: [	
+									'circa_park_stadium_1.jpg',
+									'circa_park_stadium_2.jpg',
+									'circa_park_stadium_3.jpg',
+									'circa_park_stadium_4.jpg',
+						]
+					},
+					{
+						id: 25,
+						prod_id: 15,
+						name: 'Ronnie',
+						brand: 'Rever Pass',
+						descriptions: ['Cuello volcado'],
+						color: 'Gris',
+						sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Abrigos',
+						price: 99.99,
+						images: [	
+									'rever_pass_ronnie_grey_1.jpg',
+									'rever_pass_ronnie_grey_2.jpg',
+									'rever_pass_ronnie_grey_3.jpg',
+									'rever_pass_ronnie_grey_4.jpg',
+						]
+					},
+					{
+						id: 26,
+						prod_id: 15,
+						name: 'Ronnie',
+						brand: 'Rever Pass',
+						descriptions: ['Cuello volcado'],
+						color: 'Negro',
+						sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+						ocassion: 'Casual',
+						gender: 'Hombres',
+						category: 'Indumentaria',
+						sub_category: 'Abrigos',
+						price: 99.99,
+						images: [	
+									'rever_pass_ronnie_black_1.jpg',
+									'rever_pass_ronnie_black_2.jpg',
+									'rever_pass_ronnie_black_3.jpg',
+									'rever_pass_ronnie_black_4.jpg',
+						]
+					}
+];
+
+Array.prototype.contains = function ( obj ) {
+   for (i in this) {
+       if (this[i] == obj) {
+       		return true;
+       }
+   }
+   return false;
+}
+
+function selectItemsBy(condition) {
+
+	var array = [];
+	var i;
+	for (i = 0 ; i < products.length ; i++) {
+		if(condition.call(this, products[i])) {
+			array.push(products[i]);
+		}
+	}
+	return array;
+}
+
+function getAll(field) {
+
+	var array = [];
+	var i;
+	for (i = 0 ; i < products.length ; i++) {
+		if (!array.contains(products[i][field])) {
+			array.push(register);
+		}
+	}
+	return array;
+}
 
 
 
-]
+function getByCategory(category) {
+
+	var array = [];
+
+	for (each in products) {
+		if (each.category == category) {
+			array.push(each);
+		}
+	}
+	return array;
+}
+
+
+
+
+
+
