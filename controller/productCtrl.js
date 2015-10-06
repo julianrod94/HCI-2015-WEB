e4d7b3c2ba2bb7dd3583ff0d4dc7111d56e1e4fd
jmb <juanmbellini@hotmail.com>
2015-10-06 20:26:42 -0300

    PaginaDeProductoAvanzado

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,3 +1,51 @@
+
+// Creates a JSON Object that contains filter names and values to filter
+// Based on 'filters' query string parameter
+// In case it wasn't a valid query string, returns null
+function createJSONFilterObject() {
+
+    var result = getParameterByName('filters');
+    result = decodeURIComponent(result);
+    try {
+        result = JSON.parse(result);
+    } catch (e) {
+        return null;
+    }
+    result = eval(result);
+
+    if (result.length == undefined) {
+        return null;
+    }
+
+    var i, flag = false;
+    for (i = 0 ; i < result.length & !flag ; i++) {
+        var j;
+        for (j = 0 ; j < getFieldsForFilters().length &!flag; j++) {
+            flag = result[i].name == getFieldsForFilters()[j];
+        }
+    }
+    if (!flag) {
+        return null;
+    }
+
+    return result;
+
+}
+
+// Field that contains the JSON Filter Object (we create it only once)
+var JSONFilters = createJSONFilterObject();
+
+// Returns how many filters there are (used to load a proper breadcrumb)
+function  getComponentsOfFilter() {
+    var result = JSONFilters;
+    if (result == null) {
+        return 0;
+    }
+    return result.length;
+}
+
+
+
 function getProduct() {
 
 	var prodId = getParameterByName('id');
@@ -56,11 +104,30 @@ angular.module('productApp', []).controller('productController', function($scope
 			result.push(source + array[i]);
 		}
 		return result;
+	},
+
+	$scope.get_first_picture = function(product_id) {
+
+		var source = "../../assets/clothes/t-shirts/";
+		var aux = getProductById(product_id);
+		source += aux.images[0];
+		return source;
 	}
 
-	$scope.createBackLink = function() {
+	$scope.createBackLinkSubCategory = function() {
 
-		return "catalogue.html?filters=" + encodeURIComponent(getParameterByName('filters'));
+		var newJSON = [{"name": 'gender', "values":[product_to_display.gender]}, {"name": 'category', "values":[product_to_display.category]}, {"name": 'sub', "values":[product_to_display.sub]}];
+
+
+		return "catalogue.html?filters=" + encodeURIComponent(JSON.stringify(newJSON));
+	},
+
+	$scope.createBackLinkCategory = function() {
+
+		var newJSON = [{"name": 'gender', "values":[product_to_display.gender]}, {"name": 'category', "values":[product_to_display.category]}];
+
+
+		return "catalogue.html?filters=" + encodeURIComponent(JSON.stringify(newJSON));
 	}
 })
 
