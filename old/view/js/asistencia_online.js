4d0db83f59dd701aae52b9bc5bc32b6366f344dc
sofia5 <sofia5@kth.se>
2015-10-14 00:33:10 -0300

    Footer - casi listo

---
+++ old/view/js/asistencia_online.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var text;
+
+    // Get the value of the input field with id="numb"
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+
+
+    // If x is Not a Number or less than one or greater than 10
+    if (name != "" && email != "" && phone != "" && message !="") {
+        text = "Input not valid";
+        window.alert("Tu mensaje fue mandado. Respondemos dentro de 5 dias");
+    } else {
+        text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+}/**
+ * Created by Sofia on 2015-10-14.
+ */
