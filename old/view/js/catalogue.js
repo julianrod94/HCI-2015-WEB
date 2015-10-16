b63f05c418ab88ce9d51fbd7959732d0ca2bbf47
sofia5 <sofia5@kth.se>
2015-10-16 16:32:13 -0300

    Product - hover TODO: 1. Make hover function show/hide content 2. Size buttons -> inline 3. Color btns -> change color 4. Add things to cart/fav

--- old/view/js/catalogue.js
+++ old/view/js/catalogue.js
@@ -3,6 +3,12 @@ angular.element(document).ready(function() {
 	angular.bootstrap(document, ['catalogueApp','cartApp','AccountApp']);
 });
 
+function mark(obj) {
+	obj.style.border = "1px solid black";
+};
+
+
+
 
 
 
