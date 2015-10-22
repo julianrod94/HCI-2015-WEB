df1535200fd5c8e92e4a3048208d2d2fa8b979c6
jmb <juanmbellini@hotmail.com>
2015-10-22 18:21:11 -0300

    cart

--- new/controller/headerCtrl.js
+++ new/controller/headerCtrl.js
@@ -85,6 +85,23 @@ angular.module('headerApp', []).controller('headerController', function($scope, 
 		}
 	}
 
+	function getCart() {
+
+		if (token != null) {
+			var url = "http://eiffel.itba.edu.ar/hci/service3/Order.groovy?method=GetOrderById&username=" + user_local + "&authentication_token=" + token;
+			url += "&id=" + cart;
+			console.log(url);
+			$http.get(url, {cache:true, timeout: 10000}).then(function(response) {
+				$scope.header_cart = response.data.order
+				for (var i = 0 ; i < $scope.header_cart.items.length ; i++) {
+					$scope.header_cart_total += $scope.cart.items[i].price * $scope.cart.items[i].quantity;
+				}
+
+			})
+		}
+
+	}
+
 	function signOut(){
 		if(token == null){
 			return;
@@ -99,67 +116,6 @@ angular.module('headerApp', []).controller('headerController', function($scope, 
 		});
 	}
 
-
-	function updateAccount(firstName, lastName, gender, identityCard, email, birthdate){
-		if(token == null){
-			return "Debe estar logeao para cambiar los datos de su cuenta";
-		}
-		var reName = /([^0-9]){2,80}/;
-		var ok1 =  reName.test(firstName);
-		var ok2 =  reName.test(lastName);
-		var reGender = /[FM]/;
-		var ok3 =  reGender.test(gender);
-		var reIdentityCard = /(((([0-9]){0,3})\.){0,2})([0-9]){0,3}/;
-		var ok4 = reIdentityCard.test(identityCard);
-		var ok5 = email != null;
-		var reBirthdate = /(19|20)[0-9]{2}-[01][0-9]-[0-3][0-9]/;
-		var ok6 = reBirthdate.test(birthdate);
-		var ok7 = password == repeat;
-
-		if(!ok1 || !ok2 || !ok3 || !ok4 || !ok5 || !ok6 || !ok7 ){
-			return "datos invalidos";
-		}
-		var today = new Date();
-		var dd = today.getDate();
-		var mm = today.getMonth()+1; //Enero es 0!
-		var yyyy = today.getFullYear();
-		var year = parseInt(birthdate.substring(0,3));
-		var month = parseInt(birthdate.substring(5,6));
-		var day = parseInt(birthdate.substring(8,9));
-		if(year-16 > yyyy){
-			return "Año invalido";
-		}
-		if(month>12){
-			return "mes invalido";
-		}
-		if(day > 31){
-			return "dia invalido";
-		}
-		if(year-16 == yyyy){
-			if(!month <= mm){
-				if(!day <= dd){
-					return "Tiene que ser mayor de 16 años";
-				}
-			}
-		}
-
-		var jsonDatos = {
-			firstName: firstName,
-			lastName: lastName,
-			gender: gender,
-			identityCard: identityCard,
-			email: email,
-			birthDate: birthdate,
-		}
-		var url = "eiffel.itba.edu.ar/hci/service3/Account.groovy?method=UpdateAccount&username=" + $scope.user_local + "&authentication_token=" + $scope.token + "&account=" + encodeURIComponent(JSON.stringify(jsonDatos));
-
-		$http.get(url, {cache: true, timeout: 10000}).then(function(response) {
-			if(response.hasOwnProperty("error")){
-				return response.data.error.message;
-			}
-			return "Datos de cuenta actualizados con exito";
-		})
-	}
 
 	//functionallity stuff
 	$scope.parentScope = $scope;
@@ -177,7 +133,12 @@ angular.module('headerApp', []).controller('headerController', function($scope, 
 	//account stuff
 	$scope.token = token;
 	$scope.account = null;
-	$scope.userPreferences = null;   
+	$scope.userPreferences = null; 
+
+
+	//cart stuff
+	$scope.header_cart = null;
+	$scope.header_cart_total = 0;  
 
 	$scope.getBrandLink = function(gender_id, brand) {
 
@@ -254,6 +215,7 @@ angular.module('headerApp', []).controller('headerController', function($scope, 
 	getBrands(4);
 
 	getAccount();
+	getCart()
 });
 
 angular.module('headerApp').directive('ngEnter', function() {
